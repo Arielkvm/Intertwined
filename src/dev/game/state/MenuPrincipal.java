5e785ffd421b9ab4505c987191be5f1c2a71c1ee
Kevin <00139917@uca.edu.sv>
2018-06-06 12:42:49 -0600

    Estados de juego (diferentes pantallas de juego)

---
+++ src/dev/game/state/MenuPrincipal.java
@@ -0,0 +1,16 @@
+package dev.game.state;
+
+import java.awt.Graphics;
+
+
+public class MenuPrincipal extends State{
+
+    @Override
+    public void tick() {
+    }
+
+    @Override
+    public void render(Graphics g) {
+    }
+    
+}

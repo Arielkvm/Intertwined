c05416ab901e3069903c2f6d903c09947bc122ce
Erick <00092217@uca.edu.sv>
2018-07-09 01:19:38 -0600

    Cosas

--- src/base/Conexion.java
+++ src/base/Conexion.java
@@ -1,47 +1,47 @@
-
-/**
- *
- * @author EFGK
- */
-
-import com.mysql.jdbc.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-public class Conexion {
-    private String user, pass,driver,url;
-    private Connection cnx;
-    public static Conexion instance;
-    
-    public synchronized static Conexion conectar(){
-        if(instance == null){
-            return new Conexion();
-        }
-        return instance;
-    }
-    
-    private Conexion(){
-        cargarCredenciales();
-        try{
-            Class.forName(this.driver);
-            cnx = (Connection) DriverManager.getConnection(this.url,this.user,this.pass);
-        }catch(ClassNotFoundException | SQLException ex){
-            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    private void cargarCredenciales(){
-        user = "root";
-        pass ="";
-        driver ="com.mysql.jdbc.Driver";
-        url = "jdbc:mysql://localhost/game";
-    }
-    public Connection getCnx(){
-        return cnx;
-    }
-    public void cerrarConexion(){
-        instance = null;
-    }
-}
-
+//
+///**
+// *
+// * @author EFGK
+// */
+//
+//import com.mysql.jdbc.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//
+//public class Conexion {
+//    private String user, pass,driver,url;
+//    private Connection cnx;
+//    public static Conexion instance;
+//    
+//    public synchronized static Conexion conectar(){
+//        if(instance == null){
+//            return new Conexion();
+//        }
+//        return instance;
+//    }
+//    
+//    private Conexion(){
+//        cargarCredenciales();
+//        try{
+//            Class.forName(this.driver);
+//            cnx = (Connection) DriverManager.getConnection(this.url,this.user,this.pass);
+//        }catch(ClassNotFoundException | SQLException ex){
+//            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+//    private void cargarCredenciales(){
+//        user = "root";
+//        pass ="";
+//        driver ="com.mysql.jdbc.Driver";
+//        url = "jdbc:mysql://localhost/game";
+//    }
+//    public Connection getCnx(){
+//        return cnx;
+//    }
+//    public void cerrarConexion(){
+//        instance = null;
+//    }
+//}
+//
